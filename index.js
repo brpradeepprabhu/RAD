89d62c057ebe54ae62a94b4cb62c24b7535652d6
Pradeep <brpradeepprabhu@gmail.com>
2018-09-19 07:29:05 +0530

    test for linux

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let cors = require('cors');
 let fs = require('fs');
 const projectName = 'TestMeanApp';
 const shell = require('shelljs');
-const out = 'C:/Users/seven pc1/pradeep';
+const out = '/home/pradeep/Pradeep';
 const sourceCodePath = __dirname + '/assets/code/mean.zip';
 app.use(cors());
 const decompress = require('decompress');
@@ -18,32 +18,24 @@ app.use(
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	var commands = 'mkdir ' + projectName;
-	var options = {
-		cwd: out
-	};
-	nrc.run(commands, options).then(
-		function(exitCodes) {
-			if (exitCodes != 0) {
-				res.json({ message: 'Folder already exist' });
-			} else {
-				decompress(sourceCodePath, out + '/' + projectName).then((files) => {
-					console.log('done!');
-					res.json({ files: files });
-				});
-			}
-		},
-		function(err) {
-			res.json({ message: 'error' });
-		}
-	);
+	try {
+		shell.cd(out);
+		shell.mkdir(projectName);
+
+		decompress(sourceCodePath, out + '/' + projectName).then((files) => {
+			console.log('done!');
+			res.json({ files: files });
+		});
+	} catch (e) {
+		res.json({ message: e });
+	}
 });
 app.get('/copyFiles', (req, res) => {
-    shell.cd(out + '/' + projectName + '/server');
-    shell.mkdir('models');
-	shell.mv( __dirname + '/assets/code/samplemodel.js','models/');
+	shell.cd(out + '/' + projectName + '/server');
+	shell.mkdir('models');
+	shell.mv(__dirname + '/assets/code/samplemodel.js', 'models/');
 	shell.sed('-i', 'sampleModel', 'testModel', 'models/samplemodel.js');
-    res.json({response:"files created"+__dirname + '/assets/code/samplemodel.js'})
+	res.json({ response: 'files created' + __dirname + '/assets/code/samplemodel.js' });
 });
 app.listen('3002', function() {
 	console.log('running on 3002...');
