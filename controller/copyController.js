7e6098d8cfbfe7f788e27ba18d3025d2e3eff96f
Pradeep Prabhu Bellikoth Ramesh <pbellikoth001@nam.ad.pwcinternal.com>
2018-09-19 18:27:58 +0530

    install packages

--- controller/copyController.js
+++ controller/copyController.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 const process = require('process');
 const dirName = process.cwd();
 const sourceCodePath = dirName + '/assets/code/mean.zip';
 const decompress = require('decompress');
 const fs = require('fs-extra');
 const shell = require('shelljs');
+var child_process = require('child_process');
 class CopyController {
     constructor() {
 
@@ -29,6 +29,14 @@ class CopyController {
         }
 
     }
+    installPackages(req, res) {
+        const projectName = (req.body.projectName).toString();
+        const pathName = (req.body.pathName).toString();
+        const angularPath = pathName + '/' + projectName + '/client'
+        child_process.execSync("start cmd.exe /K npm install", {
+            cwd: angularPath
+        });
+    }
     copyDataModels(req, res) {
         const projectName = (req.body.projectName).toString();
         const pathName = (req.body.pathName).toString();
@@ -40,21 +48,21 @@ class CopyController {
         fs.copy(dirName + '/assets/code/samplemodel.js', pathName + '/' + projectName + '/server/models/' + model.name + '.js')
             .then(() => {
                 let contents = fs.readFileSync(pathName + '/' + projectName + '/server/models/' + model.name + '.js', 'utf8');
-                
+
                 let replaceContent = contents.replace(new RegExp('sampleModel', 'g'), model.name);
                 fs.writeFileSync(pathName + '/' + projectName + '/server/models/' + model.name + '.js', replaceContent);
                 fs.copy(dirName + '/assets/code/samplecontroller.js', pathName + '/' + projectName + '/server/controller/' + model.name + 'Controller.js')
-                .then(() => {
-                    let contents = fs.readFileSync(pathName + '/' + projectName + '/server/controller/' + model.name + 'Controller.js', 'utf8');
-                    
-                    let replaceContent = contents.replace(new RegExp('samplemodel', 'g'), model.name);
-                    fs.writeFileSync(pathName + '/' + projectName + '/server/controller/' + model.name + 'Controller.js', replaceContent);
-                    res.json({
-                        response: replaceContent
+                    .then(() => {
+                        let contents = fs.readFileSync(pathName + '/' + projectName + '/server/controller/' + model.name + 'Controller.js', 'utf8');
+
+                        let replaceContent = contents.replace(new RegExp('samplemodel', 'g'), model.name);
+                        fs.writeFileSync(pathName + '/' + projectName + '/server/controller/' + model.name + 'Controller.js', replaceContent);
+                        res.json({
+                            response: replaceContent
+                        });
                     });
-                });
-                
-               
+
+
             })
             .catch(err => {
                 res.json({
@@ -64,6 +72,16 @@ class CopyController {
     }
 }
 var copyCtrl = new CopyController();
+/*
+{"projectName":"testAPIApp","pathName":"C:/Users/pbellikoth001/Project"}
+*/
 router.post('/project', copyCtrl.copyProject);
+/*
+{"projectName":"testAPIApp","pathName":"C:/Users/pbellikoth001/Project","model":{"name":"home"}}
+*/
 router.post('/model', copyCtrl.copyDataModels);
+/*
+{"projectName":"testAPIApp","pathName":"C:/Users/pbellikoth001/Project"}
+*/
+router.post('/install', copyCtrl.installPackages);
 module.exports = router;
